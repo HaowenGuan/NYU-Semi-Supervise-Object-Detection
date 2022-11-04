fc82807b359ab1b5f18f40de2e020996dc826f92
Haowen Guan <little.seazer@gmail.com>
2022-11-03 20:40:12 -0400

    DataSpell Win initialize YOLO

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # NYU-Deep-Learning-Final-Project
+
+The dataset of this project can be download at [here](https://drive.google.com/drive/u/1/folders/1EZxWpe66r4wclkQnrUmpp51FfK6gjE_d).
+
+According to the given labeled dataset, as an example shown above,
+
+![1](image/training_example.png)
+
+We need an object detection classification algorithm. Available options are,
+* Region-based Convolutional Neural Network ([R-CNN](https://bdtechtalks.com/2021/06/21/object-detection-deep-learning/#:~:text=Object%20detection%20networks%20provide%20both,layers%20to%20detect%20visual%20features.))
+* [Fast R-CNN](https://arxiv.org/abs/1504.08083)
+* [Faster R-CNN](https://arxiv.org/abs/1506.01497)
+* [**YOLO**](https://arxiv.org/abs/1506.02640)
+
+## Implement YOLO
+
+**TODO**
--- data_explore.py
+++ data_explore.py
@@ -1,5 +1,7 @@
 import yaml
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
 
 # i = [1, 30000]
 i = 1
@@ -10,14 +12,19 @@ with open("dataset/labeled_data/training/labels/" + str(i) + ".yml", "r") as str
     except yaml.YAMLError as exc:
         print(exc)
 
-img = plt.imread("dataset/labeled_data/training/images/" + str(i) + ".JPEG")
 
-for index in range(len(label['labels'])):
+fig, ax = plt.subplots()
+img = plt.imread("dataset/labeled_data/training/images/" + str(i) + ".JPEG")
+ax.imshow(img)
+
+n = len(label['labels'])
+colors = iter(plt.cm.rainbow(np.linspace(0, 1, n)))
+for index in range(n):
     x1, y1, x2, y2 = label['bboxes'][index]
     name = label['labels'][index]
-    plt.imshow(img[y1:y2, x1:x2])
-    plt.title(name)
-    plt.show()
+    rect = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=1, edgecolor=next(colors), facecolor='none', label=name)
+    ax.add_patch(rect)
+ax.legend()
 #%%
 max_labels = 0
 for i in range(1, 30001):
