8e5b655243318d6ac2902a6009c22c1ea05d48a2
SelenaNovelis <106102142+SelenaNovelis@users.noreply.github.com>
2022-11-17 23:42:30 -0500

    update codes in order to run eval.py

---
+++ eval.py
@@ -0,0 +1,510 @@
+# Please do not change this file.
+# We will use this file to benchmark your model.
+# If you find a bug, post it on campuswire.
+import contextlib
+import copy
+import datetime
+import errno
+import json
+import os
+import pickle
+import time
+import traceback
+from collections import defaultdict, deque
+from functools import partial
+
+import numpy as np
+import pycocotools.mask as mask_util
+import torch
+import torch.distributed as dist
+import torchvision
+import yaml
+from PIL import Image
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
+
+# Edit this line with the path to your own labeled data.
+# We will overwrite it with the hidden test set when we grade.
+VALID_DATASET_PATH = "/labeled/labeled"
+
+try:
+    # YOU MUST IMPLEMENT YOUR PROJECT IN A WAY THAT THIS WORKS
+    from model import get_model
+
+except ImportError:
+    print("WARNING: Unable to load model, using default dummy model. Reason:")
+    traceback.print_exc()
+
+    def get_model():
+        class Model(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.register_buffer("dummy", torch.tensor(1))
+
+            def forward(self, _):
+                device = self.dummy.device
+                return [
+                    {
+                        "boxes": torch.tensor(
+                            [[0, 0, 10, 10], [10, 10, 20, 20]],
+                            dtype=torch.float32,
+                            device=device,
+                        ),
+                        "labels": torch.tensor(
+                            [0, 1], dtype=torch.int32, device=device
+                        ),
+                        "scores": torch.tensor(
+                            [0.5, 0.7], dtype=torch.float32, device=device
+                        ),
+                    }
+                ]
+
+        return Model()
+
+
+class SmoothedValue(object):
+    """Track a series of values and provide access to smoothed values over a
+    window or the global series average.
+    """
+
+    def __init__(self, window_size=20, fmt=None):
+        if fmt is None:
+            fmt = "{median:.4f} ({global_avg:.4f})"
+        self.deque = deque(maxlen=window_size)
+        self.total = 0.0
+        self.count = 0
+        self.fmt = fmt
+
+    def update(self, value, n=1):
+        self.deque.append(value)
+        self.count += n
+        self.total += value * n
+
+    @property
+    def median(self):
+        d = torch.tensor(list(self.deque))
+        return d.median().item()
+
+    @property
+    def avg(self):
+        d = torch.tensor(list(self.deque), dtype=torch.float32)
+        return d.mean().item()
+
+    @property
+    def global_avg(self):
+        return self.total / self.count
+
+    @property
+    def max(self):
+        return max(self.deque)
+
+    @property
+    def value(self):
+        return self.deque[-1]
+
+    def __str__(self):
+        return self.fmt.format(
+            median=self.median,
+            avg=self.avg,
+            global_avg=self.global_avg,
+            max=self.max,
+            value=self.value,
+        )
+
+
+class MetricLogger(object):
+    def __init__(self, delimiter="\t"):
+        self.meters = defaultdict(SmoothedValue)
+        self.delimiter = delimiter
+
+    def update(self, **kwargs):
+        for k, v in kwargs.items():
+            if isinstance(v, torch.Tensor):
+                v = v.item()
+            assert isinstance(v, (float, int))
+            self.meters[k].update(v)
+
+    def __getattr__(self, attr):
+        if attr in self.meters:
+            return self.meters[attr]
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        raise AttributeError(
+            "'{}' object has no attribute '{}'".format(type(self).__name__, attr)
+        )
+
+    def __str__(self):
+        loss_str = []
+        for name, meter in self.meters.items():
+            loss_str.append("{}: {}".format(name, str(meter)))
+        return self.delimiter.join(loss_str)
+
+    def add_meter(self, name, meter):
+        self.meters[name] = meter
+
+    def log_every(self, iterable, print_freq, header=None):
+        i = 0
+        if not header:
+            header = ""
+        start_time = time.time()
+        end = time.time()
+        iter_time = SmoothedValue(fmt="{avg:.4f}")
+        data_time = SmoothedValue(fmt="{avg:.4f}")
+        space_fmt = ":" + str(len(str(len(iterable)))) + "d"
+        if torch.cuda.is_available():
+            log_msg = self.delimiter.join(
+                [
+                    header,
+                    "[{0" + space_fmt + "}/{1}]",
+                    "eta: {eta}",
+                    "{meters}",
+                    "time: {time}",
+                    "data: {data}",
+                    "max mem: {memory:.0f}",
+                ]
+            )
+        else:
+            log_msg = self.delimiter.join(
+                [
+                    header,
+                    "[{0" + space_fmt + "}/{1}]",
+                    "eta: {eta}",
+                    "{meters}",
+                    "time: {time}",
+                    "data: {data}",
+                ]
+            )
+        MB = 1024.0 * 1024.0
+        for obj in iterable:
+            data_time.update(time.time() - end)
+            yield obj
+            iter_time.update(time.time() - end)
+            if i % print_freq == 0 or i == len(iterable) - 1:
+                eta_seconds = iter_time.global_avg * (len(iterable) - i)
+                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
+                if torch.cuda.is_available():
+                    print(
+                        log_msg.format(
+                            i,
+                            len(iterable),
+                            eta=eta_string,
+                            meters=str(self),
+                            time=str(iter_time),
+                            data=str(data_time),
+                            memory=torch.cuda.max_memory_allocated() / MB,
+                        )
+                    )
+                else:
+                    print(
+                        log_msg.format(
+                            i,
+                            len(iterable),
+                            eta=eta_string,
+                            meters=str(self),
+                            time=str(iter_time),
+                            data=str(data_time),
+                        )
+                    )
+            i += 1
+            end = time.time()
+        total_time = time.time() - start_time
+        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+        print(
+            "{} Total time: {} ({:.4f} s / it)".format(
+                header, total_time_str, total_time / len(iterable)
+            )
+        )
+
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
+
+
+# fmt: off
+class_dict = {
+    "cup or mug": 0, "bird": 1, "hat with a wide brim": 2, "person": 3, "dog": 4, "lizard": 5, "sheep": 6, "wine bottle": 7,
+    "bowl": 8, "airplane": 9, "domestic cat": 10, "car": 11, "porcupine": 12, "bear": 13, "tape player": 14, "ray": 15, "laptop": 16,
+    "zebra": 17, "computer keyboard": 18, "pitcher": 19, "artichoke": 20, "tv or monitor": 21, "table": 22, "chair": 23,
+    "helmet": 24, "traffic light": 25, "red panda": 26, "sunglasses": 27, "lamp": 28, "bicycle": 29, "backpack": 30, "mushroom": 31,
+    "fox": 32, "otter": 33, "guitar": 34, "microphone": 35, "strawberry": 36, "stove": 37, "violin": 38, "bookshelf": 39,
+    "sofa": 40, "bell pepper": 41, "bagel": 42, "lemon": 43, "orange": 44, "bench": 45, "piano": 46, "flower pot": 47, "butterfly": 48,
+    "purse": 49, "pomegranate": 50, "train": 51, "drum": 52, "hippopotamus": 53, "ski": 54, "ladybug": 55, "banana": 56, "monkey": 57,
+    "bus": 58, "miniskirt": 59, "camel": 60, "cream": 61, "lobster": 62, "seal": 63, "horse": 64, "cart": 65, "elephant": 66,
+    "snake": 67, "fig": 68, "watercraft": 69, "apple": 70, "antelope": 71, "cattle": 72, "whale": 73, "coffee maker": 74, "baby bed": 75,
+    "frog": 76, "bathing cap": 77, "crutch": 78, "koala bear": 79, "tie": 80, "dumbbell": 81, "tiger": 82, "dragonfly": 83, "goldfish": 84,
+    "cucumber": 85, "turtle": 86, "harp": 87, "jellyfish": 88, "swine": 89, "pretzel": 90, "motorcycle": 91, "beaker": 92, "rabbit": 93,
+    "nail": 94, "axe": 95, "salt or pepper shaker": 96, "croquet ball": 97, "skunk": 98, "starfish": 99,
+}
+# fmt: on
+
+
+class UnlabeledDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transform):
+        r"""
+        Args:
+            root: Location of the dataset folder, usually it is /unlabeled
+            transform: the transform you want to applied to the images.
+        """
+        self.transform = transform
+
+        self.image_dir = root
+        self.num_images = len(os.listdir(self.image_dir))
+
+    def __len__(self):
+        return self.num_images
+
+    def __getitem__(self, idx):
+        # the idx of labeled image is from 0
+        with open(os.path.join(self.image_dir, f"{idx}.PNG"), "rb") as f:
+            img = Image.open(f).convert("RGB")
+
+        return self.transform(img)
+
+
+class LabeledDataset(torch.utils.data.Dataset):
+    def __init__(self, root, split, transforms):
+        r"""
+        Args:
+            root: Location of the dataset folder, usually it is /labeled
+            split: The split you want to used, it should be training or validation
+            transform: the transform you want to applied to the images.
+        """
+
+        self.split = split
+        self.transforms = transforms
+
+        self.image_dir = os.path.join(root, split, "images")
+        self.label_dir = os.path.join(root, split, "labels")
+
+        self.num_images = len(os.listdir(self.image_dir))
+
+    def __len__(self):
+        return self.num_images  # self.num_images
+
+    def __getitem__(self, idx):
+        # the idx of training image is from 1 to 30000
+        # the idx of validation image is from 30001 to 50000
+
+        if self.split == "training":
+            offset = 1
+        if self.split == "validation":
+            offset = 30001
+
+        with open(os.path.join(self.image_dir, f"{idx + offset}.JPEG"), "rb") as f:
+            img = Image.open(f).convert("RGB")
+        with open(os.path.join(self.label_dir, f"{idx + offset}.yml"), "rb") as f:
+            yamlfile = yaml.load(f, Loader=yaml.FullLoader)
+
+        num_objs = len(yamlfile["labels"])
+        # xmin, ymin, xmax, ymax
+        boxes = torch.as_tensor(yamlfile["bboxes"], dtype=torch.float32)
+        labels = []
+        for label in yamlfile["labels"]:
+            labels.append(class_dict[label])
+        labels = torch.as_tensor(labels, dtype=torch.int64)
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+
+class CocoEvaluator(object):
+    def __init__(self, coco_gt, iou_types):
+        assert isinstance(iou_types, (list, tuple))
+        coco_gt = copy.deepcopy(coco_gt)
+        self.coco_gt = coco_gt
+
+        self.iou_types = iou_types
+        self.coco_eval = {}
+        for iou_type in iou_types:
+            self.coco_eval[iou_type] = COCOeval(coco_gt, iouType=iou_type)
+
+        self.img_ids = []
+        # self.eval_imgs = {k: [] for k in iou_types}
+
+    def update(self, predictions):
+        img_ids = list(np.unique(list(predictions.keys())))
+        self.img_ids.extend(img_ids)
+
+        for iou_type in self.iou_types:
+            results = self.prepare(predictions, iou_type)
+            coco_eval = self.coco_eval[iou_type]
+
+            coco_eval.params.imgIds = list(img_ids)
+            with open(os.devnull, "w") as devnull:
+                with contextlib.redirect_stdout(devnull):
+                    # Silence stdout
+                    coco_dt = self.coco_gt.loadRes(results) if results else COCO()
+                    coco_eval.cocoDt = coco_dt
+                    coco_eval.evaluate()
+
+    def accumulate(self):
+        for coco_eval in self.coco_eval.values():
+            coco_eval.accumulate()
+
+    def summarize(self):
+        for iou_type, coco_eval in self.coco_eval.items():
+            print("IoU metric: {}".format(iou_type))
+            coco_eval.summarize()
+
+    def prepare(self, predictions, iou_type):
+        if iou_type == "bbox":
+            return self.prepare_for_coco_detection(predictions)
+        else:
+            raise ValueError("Unknown iou type {}".format(iou_type))
+
+    def prepare_for_coco_detection(self, predictions):
+        coco_results = []
+        for original_id, prediction in predictions.items():
+            if len(prediction) == 0:
+                continue
+
+            boxes = prediction["boxes"]
+            boxes = convert_to_xywh(boxes).tolist()
+            scores = prediction["scores"].tolist()
+            labels = prediction["labels"].tolist()
+
+            coco_results.extend(
+                [
+                    {
+                        "image_id": original_id,
+                        "category_id": labels[k],
+                        "bbox": box,
+                        "score": scores[k],
+                    }
+                    for k, box in enumerate(boxes)
+                ]
+            )
+        return coco_results
+
+
+def convert_to_xywh(boxes):
+    xmin, ymin, xmax, ymax = boxes.unbind(1)
+    return torch.stack((xmin, ymin, xmax - xmin, ymax - ymin), dim=1)
+
+
+def convert_to_coco_api(ds):
+    coco_ds = COCO()
+    # annotation IDs need to start at 1, not 0, see torchvision issue #1530
+    ann_id = 1
+    dataset = {"images": [], "categories": [], "annotations": []}
+    categories = set()
+    for img_idx in range(len(ds)):
+        # find better way to get target
+        # targets = ds.get_annotations(img_idx)
+        img, targets = ds[img_idx]
+        image_id = targets["image_id"].item()
+        img_dict = {}
+        img_dict["id"] = image_id
+        img_dict["height"] = img.shape[-2]
+        img_dict["width"] = img.shape[-1]
+        dataset["images"].append(img_dict)
+        bboxes = targets["boxes"]
+        bboxes[:, 2:] -= bboxes[:, :2]
+        bboxes = bboxes.tolist()
+        labels = targets["labels"].tolist()
+        areas = targets["area"].tolist()
+        iscrowd = targets["iscrowd"].tolist()
+        if "masks" in targets:
+            masks = targets["masks"]
+            # make masks Fortran contiguous for coco_mask
+            masks = masks.permute(0, 2, 1).contiguous().permute(0, 2, 1)
+        if "keypoints" in targets:
+            keypoints = targets["keypoints"]
+            keypoints = keypoints.reshape(keypoints.shape[0], -1).tolist()
+        num_objs = len(bboxes)
+        for i in range(num_objs):
+            ann = {}
+            ann["image_id"] = image_id
+            ann["bbox"] = bboxes[i]
+            ann["category_id"] = labels[i]
+            categories.add(labels[i])
+            ann["area"] = areas[i]
+            ann["iscrowd"] = iscrowd[i]
+            ann["id"] = ann_id
+            if "masks" in targets:
+                ann["segmentation"] = coco_mask.encode(masks[i].numpy())
+            if "keypoints" in targets:
+                ann["keypoints"] = keypoints[i]
+                ann["num_keypoints"] = sum(k != 0 for k in keypoints[i][2::3])
+            dataset["annotations"].append(ann)
+            ann_id += 1
+    dataset["categories"] = [{"id": i} for i in sorted(categories)]
+    coco_ds.dataset = dataset
+    coco_ds.createIndex()
+    return coco_ds
+
+
+@torch.no_grad()
+def evaluate(model, data_loader, device):
+    n_threads = torch.get_num_threads()
+    torch.set_num_threads(1)
+    cpu_device = torch.device("cpu")
+    model.eval()
+    metric_logger = MetricLogger(delimiter="  ")
+    header = "Test:"
+
+    coco = convert_to_coco_api(data_loader.dataset)
+    coco_evaluator = CocoEvaluator(coco, ["bbox"])
+
+    for images, targets in metric_logger.log_every(data_loader, 100, header):
+        images = list(img.to(device) for img in images)
+        model_time = time.time()
+
+        outputs = model(images)
+        outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
+        model_time = time.time() - model_time
+
+        res = {
+            target["image_id"].item(): output
+            for target, output in zip(targets, outputs)
+        }
+        evaluator_time = time.time()
+        coco_evaluator.update(res)
+        evaluator_time = time.time() - evaluator_time
+        metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
+
+    # accumulate predictions from all images
+    print("Averaged stats:", metric_logger)
+    coco_evaluator.accumulate()
+    coco_evaluator.summarize()
+    torch.set_num_threads(n_threads)
+    return coco_evaluator
+
+
+def main():
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    num_classes = 100
+
+    valid_dataset = LabeledDataset(
+        root=VALID_DATASET_PATH,
+        split="validation",
+        transforms=lambda x, y: (torchvision.transforms.functional.to_tensor(x), y),
+    )
+
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset,
+        batch_size=1,
+        shuffle=False,
+        num_workers=2,
+        collate_fn=collate_fn,
+    )
+
+    model = get_model().to(device)
+    evaluate(model, valid_loader, device=device)
+
+
+if __name__ == "__main__":
+    main()
--- ubteacher/modeling/meta_arch/rcnn.py
+++ ubteacher/modeling/meta_arch/rcnn.py
@@ -1,6 +1,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from detectron2.modeling.meta_arch.build import META_ARCH_REGISTRY
 from detectron2.modeling.meta_arch.rcnn import GeneralizedRCNN
+import torch
 
 
 @META_ARCH_REGISTRY.register()
@@ -9,7 +10,27 @@ class TwoStagePseudoLabGeneralizedRCNN(GeneralizedRCNN):
         self, batched_inputs, branch="supervised", given_proposals=None, val_mode=False
     ):
         if (not self.training) and (not val_mode):
-            return self.inference(batched_inputs)
+            if type(batched_inputs[0]) != dict:
+                new_batch = []
+                for x in batched_inputs:
+                    d = {"image": x}
+                    new_batch.append(d)
+                batched_inputs = new_batch
+            # return self.inference(batched_inputs)
+            predictions = self.inference(batched_inputs)
+            if len(predictions) == 1:
+                pred = {}
+                # if len(predictions[0]['instances'].get('pred_boxes')) == 0:
+                #     pred["boxes"] = torch.tensor([[1,2,3,4]])
+                #     pred["scores"] = torch.tensor([1.])
+                #     pred["labels"] = torch.tensor([2])
+                # else:
+                xmin, ymin, len_x, len_y = predictions[0]['instances'].get('pred_boxes').tensor.unbind(1)
+                pred["boxes"] = torch.stack((xmin, ymin, xmin + len_x, ymin + len_y), dim=1)
+                pred["scores"] = predictions[0]['instances'].get('scores')
+                pred["labels"] = predictions[0]['instances'].get('pred_classes')
+                return [pred]
+            return predictions
 
         images = self.preprocess_image(batched_inputs)
 
